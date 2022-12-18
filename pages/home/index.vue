66aefb34b89425087c4abf9da9844cf9ece08606
hong <kb032524@gmail.com>
2022-12-18 18:14:38 +0800

    ++

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,17 +3,6 @@
 <template>
   <div>
     回家干饭 <NuxtLink to="/">ToIndex</NuxtLink>
-    <hr />
-    <a-timeline>
-      <a-timeline-item>Create a services site 2015-09-01</a-timeline-item>
-      <a-timeline-item
-        >Solve initial network problems 2015-09-01</a-timeline-item
-      >
-      <a-timeline-item>Technical testing 2015-09-01</a-timeline-item>
-      <a-timeline-item
-        >Network problems being solved 2015-09-01</a-timeline-item
-      >
-    </a-timeline>
   </div>
 </template>
 
